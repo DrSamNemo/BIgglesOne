0d7f1c006165b0daa5476421b0e77d15210bf632
DrSamNemo <dr.samuel.nemo@gmail.com>
2012-09-04 09:37:20 -0400

    Weee

--- Program.cs
+++ Program.cs
@@ -19,7 +19,7 @@ namespace OutlookTest
             if (Process.GetProcessesByName("OUTLOOK").Count() > 0)
             {
                 app = Marshal.GetActiveObject("Outlook.Application") as Outlook.Application;
-                Console.WriteLine("Got the magical active outlook object!");
+                Console.WriteLine("Got the magical active outlook object! Hooray!");
             }
             else
             {
